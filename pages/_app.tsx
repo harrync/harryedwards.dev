2236a3890ef3bc1a824d047474dce085232f364f
Harry Edwards <harryedwards.nc@gmail.com>
2022-05-11 16:59:50 +0100

    fix build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@styles/globals.scss";
+import type { AppProps } from "next/app";
 
-function Application({ Component, pageProps }) {
+function Application({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
